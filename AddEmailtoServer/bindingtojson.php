fcaa68d336cc852425e82b09752b32efe7b989ee
zaklama <maged.zaklama@aiesec.net>
2015-04-12 18:32:09 +0200

    Removing posting data to the server

--- AddEmailtoServer/bindingtojson.php
+++ AddEmailtoServer/bindingtojson.php
@@ -7,30 +7,6 @@
 
 <?php
 
-$username = 'maged.zaklama@aiesec.net';
-$password = 'shabab2011';
-$loginUrl = 'https://auth.aiesec.org/users/sign_in';
- $fields = array(
-						'user[email]' => urlencode('maged.zaklama@aiesec.net'),
-						'user[Password]' => urlencode('shabab2011')
-				);
-
-//url-ify the data for the POST
-foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
-rtrim($fields_string, '&');
- $ch = curl_init();
-$curlConfig = array(
-    CURLOPT_URL            => "https://auth.aiesec.org/users/sign_in",
-    CURLOPT_POST           => true,
-    CURLOPT_RETURNTRANSFER => true,
-	CURLOPT_COOKIEJAR =>'cooki2e.txt',
-    CURLOPT_POSTFIELDS     => $fields_string,
-    )
-);
-curl_setopt_array($ch, $curlConfig);
-$result = curl_exec($ch);
-curl_close($ch);
-
 if(!function_exists("curl_init")) die("cURL extension is not installed");
 
 $url = 'https://gis-api.aiesec.org/v1/people.json?access_token=a1db4c66dfa700783bb922a7e4ce006c5675be7ec75aa2a975cba84a80d76f1b&page=1&per_page=1000';
